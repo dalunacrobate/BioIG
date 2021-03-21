f69fc0483e71e32edee8aa721e80f1e3b9b1a6c3
Dalunacrobate <78803631+dalunacrobate@users.noreply.github.com>
2021-03-21 01:15:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
     author="Dalunacrobate",
     install_requires=["requests","bs4"],
-    description="BioAnalyzer is a module that allows you to extract personnals informations from an instagram profile bio such as : Religion, Hobbies, Ethnicity, Emails, Paypal.me/, Snapchat, Twitter, Best Friends, Age, Location, Love Relationship, Love Relationship date, Facebooks, Astrologic Sign, Sexuality.",
+    description="BioIG is a module that allows you to extract personnals informations from an instagram profile bio such as : Religion, Hobbies, Ethnicity, Emails, Paypal.me/, Snapchat, Twitter, Best Friends, Age, Location, Love Relationship, Love Relationship date, Facebooks, Astrologic Sign, Sexuality.",
     include_package_data=True,
     url='https://github.com/dalunacrobate/BioIG',
     entry_points = {'console_scripts': ['BioIG = BioIG.core:main']},
